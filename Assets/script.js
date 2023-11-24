cf9265aff0cdfb5a25b13051a4e79bc54e8edb7b
Tyler <tylerjdwright1@gmail.com>
2023-11-24 04:06:10 -0500

    saving text value to localstorage under id working

--- Assets/script.js
+++ Assets/script.js
@@ -8,7 +8,6 @@ $(function () {
   // 9AM
   let hour9El = document.getElementById('hour-9');
   let hour9 = parseInt(hour9El.id.match(/\d+/)[0], 10);
-
   if (hour9 > currentHour){
     hour9El.classList.add('future')
   }
@@ -22,7 +21,6 @@ $(function () {
   // 10AM
   let hour10El = document.getElementById('hour-10');
   let hour10 = parseInt(hour10El.id.match(/\d+/)[0], 10);
-
   if (hour10 > currentHour){
     hour10El.classList.add('future')
   }
@@ -36,7 +34,6 @@ $(function () {
   // 11AM
   let hour11El = document.getElementById('hour-11');
   let hour11 = parseInt(hour11El.id.match(/\d+/)[0], 10);
-
   if (hour11 > currentHour){
     hour11El.classList.add('future')
   }
@@ -50,7 +47,6 @@ $(function () {
   // 12PM
   let hour12El = document.getElementById('hour-12');
   let hour12 = parseInt(hour9El.id.match(/\d+/)[0], 10);
-
   if (hour12 > currentHour){
     hour12El.classList.add('future')
   }
@@ -64,7 +60,6 @@ $(function () {
   // 1PM
   let hour13El = document.getElementById('hour-13');
   let hour13 = parseInt(hour13El.id.match(/\d+/)[0], 10);
-
   if (hour13 > currentHour){
     hour13El.classList.add('future')
   }
@@ -78,7 +73,6 @@ $(function () {
   // 2PM
   let hour14El = document.getElementById('hour-14');
   let hour14 = parseInt(hour14El.id.match(/\d+/)[0], 10);
-
   if (hour14 > currentHour){
     hour14El.classList.add('future')
   }
@@ -92,7 +86,6 @@ $(function () {
   // 3PM
   let hour15El = document.getElementById('hour-15');
   let hour15 = parseInt(hour15El.id.match(/\d+/)[0], 10);
-
   if (hour15 > currentHour){
     hour15El.classList.add('future')
   }
@@ -106,7 +99,6 @@ $(function () {
   // 4PM
   let hour16El = document.getElementById('hour-16');
   let hour16 = parseInt(hour16El.id.match(/\d+/)[0], 10);
-
   if (hour16 > currentHour){
     hour16El.classList.add('future')
   }
@@ -120,7 +112,6 @@ $(function () {
   // 5PM
   let hour17El = document.getElementById('hour-17');
   let hour17 = parseInt(hour17El.id.match(/\d+/)[0], 10);
-
   if (hour17 > currentHour){
     hour17El.classList.add('future')
   }
@@ -142,24 +133,29 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  $('.saveBtn').on('click' , function(){
-    var textareaValue = $(this).siblings('.description').val();
-    localStorage.setItem('hour-9', textareaValue);
-    console.log('Textarea value saved:', textareaValue);
-  }) 
-  
+  $(document).ready(function () {
+
+    $('.saveBtn').on('click' , function(){
+     var timeBlockEl = $(this).closest('.time-block');
+     var timeBlockId = timeBlockEl.attr('id');
+     var textArea = timeBlockEl.find('.description').val();
+     localStorage.setItem(timeBlockId, textArea);
+     console.log('Textarea value saved:', textArea, 'Time block ID key:', timeBlockId);
+    });
+
+  });
 });
 
 
 //save to refresh and maintain text entries
 //^^ add event listener to the class .saveBtn $('.saveBtn').on('click' , function(){}) + navigate the DOM to grab the val () of the textarea +save the val () of the textarea in localstorage under 'hour-9'
 
-var firstBlock = $('.time-block');
-  firstBlock.each(function(i, rand) {
-    var hourId = this.id;
-    var local = localStorage.getItem(hourId);
-    console.log(this);
+// var firstBlock = $('.time-block');
+//   firstBlock.each(function(i, rand) {
+//     var hourId = this.id;
+//     var local = localStorage.getItem(hourId);
+//     console.log(this);
     
-  });
+//   });
 
 //^^ loop  through each time block $('.time-block').each() and grab the id (ex: hour-9) + localStorage.getItem('hour-9') + set the val() of its text area to the data in localstorage
